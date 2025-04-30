ee6b32276d27d356d4b9313f9cb842cda2c9ef3b
tracer12 <vendetta12@naver.com>
2025-04-30 16:45:48 +0900

    feat: accessToken 발급 완료 후 리다이렉션

--- src/components/main/techNews/TechNews.tsx
+++ src/components/main/techNews/TechNews.tsx
@@ -4,6 +4,21 @@ const TechNews =() =>{
     return(
         <div className="technews-wrapper">
             테크뉴스 슬라이드가 들어갈 자리입니다
+            <br/>
+            asdf
+            <br/>
+            asdf
+            <br/>
+            asdf
+            <br/>
+            테크뉴스 슬라이드가 들어갈 자리입니다
+            <br/>
+            asdf
+            <br/>
+            asdf
+            <br/>
+            asdf
+            <br/>
         </div>
     )
 }
