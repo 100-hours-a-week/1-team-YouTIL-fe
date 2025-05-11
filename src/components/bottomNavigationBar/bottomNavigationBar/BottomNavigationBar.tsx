f834b9fc21c0a75376299a441a3fc06e1a557fd0
tracer12 <vendetta12@naver.com>
2025-05-11 18:29:14 +0900

    design: newTILList 헤더 height 조정

--- src/components/bottomNavigationBar/bottomNavigationBar/BottomNavigationBar.tsx
+++ src/components/bottomNavigationBar/bottomNavigationBar/BottomNavigationBar.tsx
@@ -5,16 +5,16 @@ import './BottomNavigationBar.scss';
 
 const navItems = [
   { label: '메인 페이지', href: '/' },
-  { label: 'TIL 생성', href: '/commit' },
   { label: '커뮤니티', href: '/community' },
+  { label: 'TIL 생성', href: '/commit' },
   { label: '레포지토리', href: '/repository' },
   { label: '프로필', href: '/profile' },
 ];
 
 const BottomNavigationBar = () => {
   return (
-    <div className="bottom-navigation-bar">
-      <nav className="bottom-navigation-bar__nav">
+    <div className='bottom-nav-bar'>
+      <nav className="bottom-nav">
         {navItems.map(({ label, href }) => (
           <BottomNavigationBarButton key={href} label={label} href={href} />
         ))}
