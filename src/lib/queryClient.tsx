56c9a04b87df0165f4614c51f8a8c93a212206ec
tracer12 <vendetta12@naver.com>
2025-05-25 21:22:26 +0900

    feat: query Client defaultOption 추가

--- src/lib/queryClient.tsx
+++ src/lib/queryClient.tsx
@@ -1,3 +1,12 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 3600000,
+      gcTime: 3600000,
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
