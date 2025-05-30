3b07c0c46a9a18bd931a944bb38ec96d342594bc
tracer12 <vendetta12@naver.com>
2025-05-30 20:29:24 +0900

    feat: 미들웨어 도입
    쿠키에 refreshToken이 없으면 /login으로 리다이렉션 설정, AuthGuard 가 작동하지 않음
    만약 accessToken은 없고 refreshToken만 있다면 authGuard가 막아줌
    이 경우는 추후 로그아웃 api가 만들어지면 전부 middleware에서 처리하도록 수정

--- src/app/layoutWrapper/LayoutWrapper.tsx
+++ src/app/layoutWrapper/LayoutWrapper.tsx
@@ -1,52 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Header from '@/components/header/header/Header';
 import BottomNavigationBar from '@/components/bottomNavigationBar/bottomNavigationBar/BottomNavigationBar';
 import AuthGuard from '@/components/authGuard/AuthGuard';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '../../lib/queryClient';
-import useGetAccessToken from '@/hooks/useGetAccessToken';
-
 import './LayoutWrapper.scss';
 
-const VALID_ROUTES = [
-  '/',
-  '/login',
-  '/commit',
-  '/generate',
-  '/repository',
-  '/profile',
-  '/community',
-];
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../../lib/queryClient';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const router = useRouter();
-  const accessToken = useGetAccessToken();
-
   const isLoginPage = pathname === '/login';
-  const isValidRoute = VALID_ROUTES.includes(pathname);
-
-  useEffect(() => {
-    if (!isValidRoute) {
-      if (accessToken) {
-        router.replace('/');
-      } else {
-        router.replace('/login');
-      }
-    }
-  }, [isValidRoute, accessToken, router]);
 
   return (
     <QueryClientProvider client={queryClient}>
       <div className="layout__container">
         <div className="layout__frame">
           {!isLoginPage && <Header />}
+
           <main className="layout__main">
             {isLoginPage ? children : <AuthGuard>{children}</AuthGuard>}
           </main>
+
           {!isLoginPage && <BottomNavigationBar />}
         </div>
       </div>
--- src/components/header/headerLogo/HeaderLogo.tsx
+++ src/components/header/headerLogo/HeaderLogo.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const HeaderLogo = () => {
+
   return (
     <Link href="/" className="header-logo">
       <Image
