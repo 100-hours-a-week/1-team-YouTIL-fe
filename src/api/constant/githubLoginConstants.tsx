ca4a5ad513e3c6682842b2a00a9804263d776b2f
tracer12 <vendetta12@naver.com>
2025-05-30 14:49:29 +0900

    feat: Oauth 로그인 시 쿼리에 state값 삽입
    세션 스토리지에 저장된 state값과 비교하여 클라이언트에서 CSRF 공격을 방어

--- src/api/constant/githubLoginConstants.tsx
+++ src/api/constant/githubLoginConstants.tsx
@@ -1,7 +1,13 @@
 export const GITHUB_AUTH_SCOPE =
   'read:user user:email read:org repo';
 
-export const getGithubAuthUrl = (clientId: string) =>
-  `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${encodeURIComponent(
-    GITHUB_AUTH_SCOPE
-  )}`;
+  const generateState = () => window.crypto.randomUUID();
+
+  export const getGithubAuthUrl = (clientId: string) => {
+    const state = generateState();
+    sessionStorage.setItem('oauth_state', state);
+  
+    return `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${encodeURIComponent(
+      GITHUB_AUTH_SCOPE
+    )}&state=${state}`;
+  };
--- src/components/login/githubLoginButton/GithubLoginButton/GithubLoginButton.tsx
+++ src/components/login/githubLoginButton/GithubLoginButton/GithubLoginButton.tsx
@@ -1,26 +1,35 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useGithubLogin } from '@/hooks/useGithubLogin';
 import LoginButton from '../LoginButton/LoginButton';
 
 const GithubLoginButton = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const { login } = useGithubLogin();
 
   useEffect(() => {
     const code = searchParams.get('code');
+    const returnedState = searchParams.get('state');
+    const expectedState = sessionStorage.getItem('oauth_state');
+
     if (!code) return;
 
+    if (!expectedState || returnedState !== expectedState) {
+      console.log('state 검증 실패');
+      return;
+    }
+    
     login(code)
       .then(() => {
-        window.location.href = '/';
+        router.replace('/');
       })
       .catch((error) => {
         console.error('GitHub 로그인 실패:', error);
       });
-  }, [searchParams, login]);
+  }, [searchParams, login, router]);
 
   return <LoginButton />;
 };
