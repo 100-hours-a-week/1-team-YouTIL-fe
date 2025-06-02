fb7ba769b95f3851a0ebf94b0ae7cc8a0da73fc1
tracer12 <vendetta12@naver.com>
2025-06-02 17:12:48 +0900

    test: refreshToken 없을 시 에러 출력 삭제

--- src/components/refreshAccess/RefreshAccess.tsx
+++ src/components/refreshAccess/RefreshAccess.tsx
@@ -8,16 +8,20 @@ const RefreshAccess = () => {
 
   useEffect(() => {
     const checkAndRefresh = async () => {
+      try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/news`, {
           method: 'GET',
           credentials: 'include',
         });
-        if (res.status === 401) {
+
+        if (res.ok) {
           const newAccessToken = res.headers.get("Authorization")?.replace("Bearer ", '');
           if (newAccessToken) {
             setAccessToken(newAccessToken);
           }
         }
+      } catch {
+      }
     };
 
     checkAndRefresh();
