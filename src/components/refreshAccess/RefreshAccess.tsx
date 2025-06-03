a06aa074b01bf90fea2d9091181f327d247eaf95
tracer12 <vendetta12@naver.com>
2025-06-03 14:56:38 +0900

    test: 디버깅용 console.log 제거

--- src/components/refreshAccess/RefreshAccess.tsx
+++ src/components/refreshAccess/RefreshAccess.tsx
@@ -8,26 +8,21 @@ const RefreshAccess = () => {
 
   useEffect(() => {
     const checkAndRefresh = async () => {
-      console.log("asdf");
       try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/news`, {
           method: 'GET',
           credentials: 'include',
         });
-        console.log("res = ", res);
         if (res.status === 401) {
           const newAccessToken = res.headers.get("Authorization")?.replace("Bearer ", '');
           console.log(newAccessToken);
           if (newAccessToken) {
-            console.log(newAccessToken);
             setAccessToken(newAccessToken);
           } else {
             console.warn("401 발생, accessToken 없음");
           }
         }
-      } catch (err) {
-        console.warn("refresh 요청 실패 (무시 가능):", err);
-        console.log("요청 실패(catch)")
+      } catch {
       }
     };
 
