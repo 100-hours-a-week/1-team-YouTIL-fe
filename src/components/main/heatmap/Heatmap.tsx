67796b6de5fe9f9f2d01db942bf2e9f924a9b652
tracer12 <vendetta12@naver.com>
2025-06-05 22:27:38 +0900

    feat: 히트맵 날짜 이동 가능 여부에 따라 버튼 비활성화

--- src/components/main/heatmap/Heatmap.tsx
+++ src/components/main/heatmap/Heatmap.tsx
@@ -179,13 +179,13 @@ const Heatmap = () => {
       <div className="heatmap-wrapper__container">
         <div className="heatmap-wrapper__content">
           <div className="heatmap-wrapper__controls-left">
-            <button onClick={handlePrevDomain}>&lt;</button>
+            <button  className={prevStep === 0 ? 'disabled' : ''} onClick={handlePrevDomain}>&lt;</button>
           </div>
 
           <div id="ex-ghDay" className="heatmap-wrapper__calendar"></div>
 
           <div className="heatmap-wrapper__controls-right">
-            <button onClick={handleNextDomain}>&gt;</button>
+            <button className={nextStep === 0 ? 'disabled' : ''} onClick={handleNextDomain} >&gt;</button>
           </div>
         </div>
 
