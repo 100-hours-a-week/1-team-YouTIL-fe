7436a6f8edd9e3207f4c0897cbead2443bf5d174
tracer12 <vendetta12@naver.com>
2025-06-06 20:31:29 +0900

    feat: 유저 프로필 사진 및 자기소개 출력

--- src/components/profile/userNickNameDescription/UserNickNameDescription.tsx
+++ src/components/profile/userNickNameDescription/UserNickNameDescription.tsx
@@ -1,9 +1,18 @@
-const UserNickNameDescription =() =>{
-    return(
-        <div>
-            유저 닉네임
-        </div>
-    )
-}
+'use client';
+
+import './UserNickNameDescription.scss';
+import useUserInfoStore from '@/store/useUserInfoStore';
+
+const UserNickNameDescription = () => {
+  const name = useUserInfoStore((state) => state.userInfo.name);
+
+  return (
+    <div className="user-nickname">
+      <div className="user-nickname__box">
+        {name ? `${name} 님의 마이페이지` : '닉네임 정보 없음'}
+      </div>
+    </div>
+  );
+};
 
 export default UserNickNameDescription;
--- src/components/profile/userProfileInfo/UserProfileInfo.tsx
+++ src/components/profile/userProfileInfo/UserProfileInfo.tsx
@@ -1,9 +1,25 @@
-const UserProfileInfo =() =>{
-    return(
-        <div>
-            유저 프로필 정보
-        </div>
-    )
-}
+'use client';
+
+import './UserProfileInfo.scss';
+import useUserInfoStore from '@/store/useUserInfoStore';
+
+const UserProfileInfo = () => {
+  const { profileUrl, description } = useUserInfoStore((state) => state.userInfo);
+
+  return (
+    <div className="user-profile">
+      {profileUrl && (
+        <img
+          src={profileUrl}
+          alt="유저 프로필 이미지"
+          className="user-profile__image"
+        />
+      )}
+      <div className="user-profile__introduction">
+        {description ?? '유저 소개 없음'}
+      </div>
+    </div>
+  );
+};
 
 export default UserProfileInfo;
