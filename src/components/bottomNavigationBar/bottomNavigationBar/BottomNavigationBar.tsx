d60ec21cd5927830a73c087ba184836570b1bab9
tracer12 <vendetta12@naver.com>
2025-06-06 22:18:17 +0900

    feat: 바텀 네비게이션 바 프로필 페이지 라우팅 방식 동적으로 변경

--- src/components/bottomNavigationBar/bottomNavigationBar/BottomNavigationBar.tsx
+++ src/components/bottomNavigationBar/bottomNavigationBar/BottomNavigationBar.tsx
@@ -2,16 +2,19 @@
 
 import BottomNavigationBarButton from '../bottomNavigationBarButton/BottomNavigationBarButton';
 import './BottomNavigationBar.scss';
-
-const navItems = [
-  { label: '메인 페이지', href: '/' },
-  { label: '커뮤니티', href: '/community' },
-  { label: 'TIL 생성', href: '/commit' },
-  { label: '레포지토리', href: '/repository' },
-  { label: '프로필', href: '/profile' },
-];
+import useUserInfoStore from '@/store/useUserInfoStore';
 
 const BottomNavigationBar = () => {
+  const userId = useUserInfoStore((state) => state.userInfo.userId);
+
+  const navItems = [
+    { label: '메인 페이지', href: '/' },
+    { label: '커뮤니티', href: '/community' },
+    { label: 'TIL 생성', href: '/commit' },
+    { label: '레포지토리', href: '/repository' },
+    { label: '프로필', href: `/profile/${userId}`},
+  ];
+
   return (
     <div className="bottom-nav__container">
       <nav className="bottom-nav__nav">
