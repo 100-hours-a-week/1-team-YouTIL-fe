cd2ee3abed4f4b8406a4e5e99dd9c68faae5897b
tracer12 <vendetta12@naver.com>
2025-06-10 14:17:05 +0900

    design: 면접 질문 난이도<->제목 위치 변경

--- src/components/repository/interview/RepositoryInterviewList.tsx
+++ src/components/repository/interview/RepositoryInterviewList.tsx
@@ -154,9 +154,9 @@ const RepositoryInterviewList = () => {
                   onClick={() => handleClickInterview(interview.id)}
                 >
                   <div className="repository-interview-list__item-header-top">
-                    <h3 className={`repository-interview-list__item-title${ isExpanded ? ' repository-interview-list__item-title--expanded' : ''}`}>
-                      {interview.title} ({mapLevelToLabel(interview.level)})
-                    </h3>
+                  <h3 className={`repository-interview-list__item-title${ isExpanded ? ' repository-interview-list__item-title--expanded' : ''}`}>
+                    ({mapLevelToLabel(interview.level)}) {interview.title}
+                  </h3>
                   </div>
                   <p className="repository-interview-list__item-date">{formattedDate}</p>
                 </div>
