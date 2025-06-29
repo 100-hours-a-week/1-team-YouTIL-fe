0dceb951d7f8a9200842145276d3daab4f3a2d85
tracer12 <vendetta12@naver.com>
2025-06-29 21:07:07 +0900

    feat: 유저 정보 불러올 시 쿼리키 팩토리 적용

--- src/querykey/main.querykey.ts
+++ src/querykey/main.querykey.ts
@@ -1,6 +1,7 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
 export const mainKeys = createQueryKeys('main', {
+    userInfo: () => ['userInfo'], // 메인화면 렌더링 시 유저 정보
     newTILList: () => ['til-recent'], // 메인화면 NewTILList
     heatmapCalendar: (year: number) => ['til-heatmap', year], //메인화면 히트맵 캘린더
   });
