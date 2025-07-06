1ccc62cd7cd62a34888f1c6d4cc4bdba6260dd16
tracer12 <vendetta12@naver.com>
2025-07-06 17:34:53 +0900

    test: 브렌치 충돌 테스트

--- src/querykey/community.querykey.ts
+++ src/querykey/community.querykey.ts
@@ -1,6 +1,6 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
 export const communityKeys = createQueryKeys('community', {
-    communityComment:(tilIdNumber:number) => ['comment', tilIdNumber],
+    communityComment:(tilIdNumber:number) => ['comment', tilIdNumber], // 브랜치 충돌 테스트
 });
 
