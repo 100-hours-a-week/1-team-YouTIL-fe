99dfb4d9021b9e876979b64397175bfdc8e06fc0
tracer12 <vendetta12@naver.com>
2025-07-07 15:24:09 +0900

    feat: 각 쿼리키 정보 주석 추가

--- src/querykey/commit.querykey.ts
+++ src/querykey/commit.querykey.ts
@@ -1,7 +1,7 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 
 export const commitKeys = createQueryKeys('commit', {
-  commitCalendar: (
+  commitCalendar: ( // 커밋 리스트 캘린더 쿼리키
     organizationId: string | number,
     repositoryId: number | undefined,
     branchName: string | undefined,
@@ -14,10 +14,10 @@ export const commitKeys = createQueryKeys('commit', {
       year
     ],
   }),
-  organization:() => ['static'],
-  repository: (organizationId: string | number) => [organizationId || ''],
-  branch:(organization_id : string | number, repositoryId : number | undefined) => [organization_id || '', repositoryId],
-  list :(organization_id : string | number, 
+  organization:() => ['static'], // 선택한 organization 쿼리키
+  repository: (organizationId: string | number) => [organizationId || ''], // 선택한 레포지토리 쿼리키
+  branch:(organization_id : string | number, repositoryId : number | undefined) => [organization_id || '', repositoryId], // 선택한 브랜치 쿼리키
+  list :(organization_id : string | number, // 커밋 리스트 쿼리키
     repositoryId : number | undefined, 
     branchName : string | undefined, 
     selectedDate : string) => [organization_id || '', repositoryId, branchName, selectedDate],
--- src/querykey/community.querykey.ts
+++ src/querykey/community.querykey.ts
@@ -1,8 +1,8 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
 export const communityKeys = createQueryKeys('community', {
-    comment:(tilIdNumber:number) => [tilIdNumber],
-    list:(selectedCategory:string) => [selectedCategory],
-    detail:(tilIdNumber : number) => [tilIdNumber],
+    comment:(tilIdNumber:number) => [tilIdNumber], // tilDetail(커뮤니티) 댓글 쿼리키
+    list:(selectedCategory:string) => [selectedCategory], // 커뮤니티 리스트 쿼리키
+    detail:(tilIdNumber : number) => [tilIdNumber], // tilDetail(커뮤니티) 쿼리키
 });
 
